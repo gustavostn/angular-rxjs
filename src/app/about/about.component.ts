00bb8839e44eb4eb5bdc6681d2dabb131647e50e
gustavo.carvalho <gustavo.stn@outlook.com.br>
2022-10-17 22:51:03 -0300

    feat: Funcionalidades iniciais do rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,33 +1,33 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
     selector: 'about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+    ngOnInit() { 
+        /*
+        -- Interval com rxjs
+        const interval$ = interval(1000)
+        interval$.subscribe(value => console.log("Stream 1:", value))
+        interval$.subscribe(value => console.log("Stream 2:", value))
+        */
+        
+        /*
+        -- Executa o time (1000) após o tempo inicial (3000)
+        const interval$ = timer(3000, 1000);
+        interval$.subscribe(value => console.log("Time: ", value))
+        */
 
-
+        /* 
+        -- Escutar eventos com o rxjs
+        const click$ = fromEvent(document, 'click')
+        click$.subscribe(evt => console.log(evt))
+        */
     }
 
 
