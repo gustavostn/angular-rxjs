24229699acdf78cc5742a1fda35aef0ba0dce389
gustavo.carvalho <gustavo.stn@outlook.com.br>
2022-10-20 00:35:54 -0300

    feat: Utilizando startWith p/ setar o valor da primeira requisição

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { concat, fromEvent, Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
 import { callHtpp } from '../common/util';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
@@ -22,13 +22,15 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     private _idCourse: number = 0
 
-    constructor(private _activatedRouter: ActivatedRoute) {}
+    constructor(private _activatedRouter: ActivatedRoute) { }
 
     ngOnInit(): void {
         this._idCourse = this._activatedRouter.snapshot.params.id
         this._getCourseInfos(this._idCourse)
     }
 
+    // Neste caso é utilizado o concat p/ formar o valor inicial do lessons$
+    /*
     ngAfterViewInit(): void {
         this.searchBarEvent$ = fromEvent(this._searchInput.nativeElement, "keyup")
             .pipe(
@@ -39,6 +41,19 @@ export class CourseComponent implements OnInit, AfterViewInit {
             )
         const initialLessonsValue$ = this._getLessons(this._idCourse, '')
         this.lessons$ = concat(initialLessonsValue$, this.searchBarEvent$)
+    }
+    */
+
+    // Neste caso estamos setando que o valor inicial da busca será uma string vazia
+    ngAfterViewInit(): void {
+        this.lessons$ = fromEvent(this._searchInput.nativeElement, "keyup")
+            .pipe(
+                map((event: any) => event.target.value),
+                startWith(''),
+                debounceTime(400), // Aguarda 400ms p/ emitir um novo evento
+                distinctUntilChanged(), // So emite evento caso tenha diferença no valor
+                switchMap(search => this._getLessons(this._idCourse, search)) // Caso receba uma nova informação cancela o evento antigo | Neste caso, quando recebe um novo valor, cancela a requisição antiga
+            )
     }
 
     private _getCourseInfos(idCourse: number): void {
